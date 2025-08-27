2ac9c9a270fddaec7b77e583f8b7a54595e07c1b
Sebastian <jalfarg@gmail.com>
2025-08-27 15:40:47 -0300

    call to action section implemented

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -12,6 +12,7 @@ import { FAQ } from "@/sections/FAQ";
 import { Works } from "@/sections/works";
 import { Benefits } from "@/sections/benefits";
 import { FeaturedTestimonial } from "@/sections/featured-testimonial";
+import { CallToAction } from "@/sections/call-to-action";
 
 
 function Homepage() {
@@ -48,13 +49,20 @@ function Homepage() {
 
     <FAQ />
 
+    <CallToAction
+      title="Un titulo de ejemplo y banners en 3d."
+      description="Descripcion de refuerzo para el call to action, con banners en 3d para reforzar el trabajo bien hecho."
+      button="Texto call to action"
+      image="src/assets/examples/banner-kavak.webp"
+    />
+
     {/* Blur effect */}
-    <div className="fixed inset-x-0 bottom-0 h-40 backdrop-blur-3xl z-30"
+    {/* <div className="fixed inset-x-0 bottom-0 h-40 backdrop-blur-3xl z-30"
       style={{
         maskImage: 'linear-gradient(to top, black, transparent)',
         WebkitMaskImage: 'linear-gradient(to top, black, transparent)'
       }}>
-    </div>
+    </div> */}
   </main>
 }
 
